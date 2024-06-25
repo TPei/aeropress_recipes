f1b12bb08a659c84fb6531373dcb51faf9ea6cde
Thomas Karstens <t.s.peikert@gmail.com>
2024-06-25 23:21:31 +0200

    simple version of recipe generation

---
+++ database.js
@@ -0,0 +1,19 @@
+const database = {
+  brewTemperature: [
+    "80 degrees", "81 degrees", "84 degrees", "85 degrees", "90 degrees", "95 degrees", "100 degrees"
+  ],
+  coffeeToWater: [
+    "15g coffee : 200g water", "18g coffee : 250g water", "20g coffee : 300g water",
+    "17g coffee : 220g water", "35g coffee : 150g water", "30g coffee : 220g water"
+  ],
+  grindSizeAndBrewTime: [
+    "Coarse grind, 3 minutes", "Medium grind, 2.5 minutes", "Fine grind, 2 minutes",
+    "Coarse grind, 2:30 minutes", "Medium grind, 1:18 minutes", "Fine grind, 1:45 minutes"
+  ],
+  brewTechnique: [
+    "Standard", "Inverted", "Pre-Infusion", "Bloom", "Continuous pour", "Bypass brewing"
+  ],
+  stirringTechnique: [
+    "No stir", "Stir once", "Stir twice", "Stir continuously", "Swirl contents", "Vigorously stir"
+  ]
+};
