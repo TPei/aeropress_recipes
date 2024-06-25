0a81454e5c25ead365e7d6877a5fd17dae2917bd
Thomas Karstens <t.s.peikert@gmail.com>
2024-06-25 23:59:10 +0200

    some UX tinkering

--- database.js
+++ database.js
@@ -3,15 +3,16 @@ const database = {
     "80 degrees", "81 degrees", "84 degrees", "85 degrees", "90 degrees", "95 degrees", "100 degrees"
   ],
   coffeeToWater: [
-    "15g coffee : 200g water", "18g coffee : 250g water", "20g coffee : 300g water",
+    "15g coffee : 200g water", "15g coffee : 250g water", "20g coffee : 300g water",
     "17g coffee : 220g water", "35g coffee : 150g water", "30g coffee : 220g water"
   ],
   grindSizeAndBrewTime: [
     "Coarse grind, 3 minutes", "Medium grind, 2.5 minutes", "Fine grind, 2 minutes",
-    "Coarse grind, 2:30 minutes", "Medium grind, 1:18 minutes", "Fine grind, 1:45 minutes"
+    "Coarse grind, 2:30 minutes", "Medium grind, 1:18 minutes", "Fine grind, 1:45 minutes",
+    "Fine grind, 60 seconds"
   ],
   brewTechnique: [
-    "Standard", "Inverted", "Pre-Infusion", "Bloom", "Continuous pour", "Bypass brewing"
+    "Standard - 30s Bloom - 60g Water", "Inverted", "Pre-Infusion", "Bloom", "Continuous pour", "Bypass brewing"
   ],
   stirringTechnique: [
     "No stir", "Stir once", "Stir twice", "Stir continuously", "Swirl contents", "Vigorously stir"
